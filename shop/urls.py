5f688886900f326a978df940f8a4618e83c1dce3
pasharad <pasharad2231@gmail.com>
2023-05-26 11:46:40 +0330

    detail developed

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
-from .views import index
+from .views import index, product_view
 from django.urls import path
 
 urlpatterns = [
-    path('index', index, name='index')
+    path('index/', index, name='index'),
+    path('product-detail/<int:pk>', product_view, name='product-detail')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from .models import Categorys, Products, Discounts
+from .models import Categorys, Colors, Image, Products, Discounts, InventoryProduct
 
 # Create your views here.
 
@@ -24,4 +24,28 @@ def index(request):
                 price_list.append(a)
 
     content = render(request, 'index.html', {'categorys':categorys, 'price':price_list})
+    return HttpResponse(content)
+
+def product_view(request, pk):
+    product = Products.objects.get_one(pk=pk)
+    product_inventory = InventoryProduct.objects.filter(product_id = product.id)
+    colors = []
+    for p in product_inventory:
+        color = Colors.objects.get(pk=p.color)
+        colors.append(color)
+    discount = Discounts.objects.get(products=product)
+    if discount.type == 'CSH':
+        price_after_discount = int(product.price) - discount.amount
+    elif discount.type == 'PER':
+        price_after_discount = int(product.price) * (1 - discount.amount)
+
+    image = Image.objects.filter(product=product)
+    content = render(request, 'detail.html', 
+                     {
+                        'product':product,
+                        'product_inventory':product_inventory,
+                        'colors':colors,
+                        'discounted_price': price_after_discount,
+                        'img':image
+                    })
     return HttpResponse(content)
